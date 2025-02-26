fe394bc478e4c38a22bb996627eb47d2caabe9d2
gcomm <gcom@uw.edu>
2025-02-26 12:10:54 -0800

    commented out more docs

---
+++ doc/example-usage.R
@@ -0,0 +1,19 @@
+## ----include = FALSE----------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ----installation-------------------------------------------------------------
+library(pets)
+
+## ----setup--------------------------------------------------------------------
+## load pets package
+library(pets)
+
+## if you love cats
+cats(TRUE)
+
+## if you're not a big fan of cats
+cats(FALSE)
+
